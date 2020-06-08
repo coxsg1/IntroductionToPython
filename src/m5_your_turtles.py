29d76613facc51631d18ec9ef2e5371288afa709
coxsg1 <coxsg1@rose-hulman.edu>
2020-06-08 10:46:05 -0400

    Blue and Purple Turtles

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Sarah Cox.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -20,6 +20,50 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #    2. Each rg.SimpleTurtle object draws something
 #         (by moving, using its rg.Pen).  ANYTHING is fine!
 #    3. Each rg.SimpleTurtle moves inside a LOOP.
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+purple_turtle = rg.SimpleTurtle('turtle')
+purple_turtle.pen_up()
+purple_turtle.go_to(rg.Point(100,30))
+purple_turtle.pen_down()
+purple_turtle.pen = rg.Pen('purple', 3)
+purple_turtle.speed = 100  # Fast
+size = 100
+for k in range(20):
+
+    purple_turtle.draw_square(size)
+
+    purple_turtle.pen_up()
+    purple_turtle.right(45)
+    purple_turtle.forward(20)
+    purple_turtle.left(45)
+
+    purple_turtle.pen_down()
+    size = size - 8
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen_up()
+blue_turtle.go_to(rg.Point(-100,-60))
+blue_turtle.pen_down()
+blue_turtle.pen = rg.Pen('blue',3)
+blue_turtle.speed = 100
+size = 20
+for k in range(11):
+
+    blue_turtle.draw_regular_polygon(size, size)
+
+    blue_turtle.pen_up()
+    blue_turtle.right(45)
+    blue_turtle.forward(8)
+    blue_turtle.left(45)
+
+    blue_turtle.pen_down()
+    size = size - 2
+
+
+
 #
 #  Be creative!  Strive for way-cool pictures!  Abstract pictures rule!
 #
